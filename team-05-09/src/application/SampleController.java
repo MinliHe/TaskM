ad9e57880c61526df95e7e0a72942c1d70b1982d
kwanQing <jeffrey44583@gmail.com>
2021-03-29 17:47:11 +0800

    Change some designs on it

--- team-05-09/src/application/SampleController.java
+++ team-05-09/src/application/SampleController.java
@@ -7,15 +7,16 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyEvent;
 
 public class SampleController {
+	
+	public Button signupB;
 	
 	private ArrayList<String> list = new ArrayList<>();
 	@FXML Button add;
 	@FXML TextField task;
 	@FXML ListView taskView;
+
 	
 // TESTING ADD BUTTON
 //	public void add(String message) {
@@ -23,7 +24,7 @@ public class SampleController {
 //		System.out.println("Task: " + message);
 //	}
 	
-//TESTING ENTER BUTTON
+// TESTING ENTER BUTTON
 //	public void enterTest(KeyEvent event) {
 //		if(event.getCode().equals(KeyCode.ENTER))
 //		{
@@ -35,18 +36,16 @@ public class SampleController {
 	//Adds task to taskView and displays it
 	public void onAdd(ActionEvent event) {
 		
-		if(task.getText() != null && task.getText().length() > 0) //check if a task has been inputted
+		if(task.getText() != null && task.getText().length() > 0) //check if a task has been inputed
 		{
 			list.add(task.getText()); //add task object to list for later features
 			taskView.getItems().add((String)task.getText()); // add task to task view
 			task.clear(); //clear input for new task
 		}
-	
-
-		
-
 	}
+
 	
+    
 // TESTING DISPLAY OF TASKVIEW
 //	public void display() {
 //		System.out.println("Displaying List of Tasks");
